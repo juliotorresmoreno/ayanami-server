17ed820f6a6e1c3585534c58fea2ee00e1db7493
Julio Torres <jtorres990@gmail.com>
2025-02-24 14:58:34 -0500

    Refactor ProfileController for improved code readability and consistency; adjust indentation and formatting

--- src/main/java/us/onnasoft/ayanami/controller/ProfileController.java
+++ src/main/java/us/onnasoft/ayanami/controller/ProfileController.java
@@ -14,56 +14,56 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/profile")
 public class ProfileController {
-  private final ProfileService profileService;
+    private final ProfileService profileService;
 
-  @Autowired
-  public ProfileController(ProfileService profileService) {
-    this.profileService = profileService;
-  }
-
-  /**
-   * Obtiene el perfil del usuario autenticado.
-   * 
-   * @return El perfil del usuario si existe, o un 404 si no.
-   */
-  @GetMapping("")
-  public ResponseEntity<Object> getUserProfile() {
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    String username = authentication.getName();
-
-    User user = profileService.getUserProfileByUsername(username);
-    if (user != null) {
-      return ResponseEntity.ok(user);
-    } else {
-      return ResponseEntity.notFound().build();
-    }
-  }
-
-  /**
-   * Actualiza el perfil de un usuario.
-   * 
-   * @param userId      El ID del usuario.
-   * @param updatedUser Los datos actualizados del usuario.
-   * @return El perfil actualizado.
-   */
-  @PutMapping("")
-  public ResponseEntity<User> updateUserProfile(@Valid @RequestBody ProfileUpdateRequest payload) {
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    String username = authentication.getName();
-
-    User user = profileService.getUserProfileByUsername(username);
-    if (user == null) {
-      return ResponseEntity.notFound().build();
+    @Autowired
+    public ProfileController(ProfileService profileService) {
+        this.profileService = profileService;
     }
 
-    user.setName(payload.getName());
-    user.setPhone(payload.getPhone());
-    user.setBio(payload.getBio());
-    user.setLocation(payload.getLocation());
-    user.setWebsite(payload.getWebsite());
-    user.setBirthDate(payload.getBirthDate());
-    user.setGender(payload.getGender());
+    /**
+     * Obtiene el perfil del usuario autenticado.
+     * 
+     * @return El perfil del usuario si existe, o un 404 si no.
+     */
+    @GetMapping("")
+    public ResponseEntity<Object> getUserProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
 
-    return ResponseEntity.ok(profileService.updateUserProfile(user.getId(), user));
-  }
+        User user = profileService.getUserProfileByUsername(username);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /**
+     * Actualiza el perfil de un usuario.
+     * 
+     * @param userId      El ID del usuario.
+     * @param updatedUser Los datos actualizados del usuario.
+     * @return El perfil actualizado.
+     */
+    @PutMapping("")
+    public ResponseEntity<User> updateUserProfile(@Valid @RequestBody ProfileUpdateRequest payload) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        User user = profileService.getUserProfileByUsername(username);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        user.setName(payload.getName());
+        user.setPhone(payload.getPhone());
+        user.setBio(payload.getBio());
+        user.setLocation(payload.getLocation());
+        user.setWebsite(payload.getWebsite());
+        user.setBirthDate(payload.getBirthDate());
+        user.setGender(payload.getGender());
+
+        return ResponseEntity.ok(profileService.updateUserProfile(user.getId(), user));
+    }
 }
